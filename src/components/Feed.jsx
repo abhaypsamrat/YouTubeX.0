afda8d37c234ce6f2147c6e80e400edf8990f5d9
abhaypsamrat <abhay.p.samrat@gmail.com>
2023-02-10 11:07:37 +0530

    set category vise videos

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,7 +2,15 @@ import { useState, useEffect } from "react";
 import { Box, Stack, Typography, typography } from "@mui/material";
 import { Sidebar, Videos } from "./";
 
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box
@@ -12,7 +20,10 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        <Sidebar />
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Typography
           className="copyright"
           variant="body2"
@@ -28,7 +39,7 @@ const Feed = () => {
           mb={2}
           sx={{ color: "white" }}
         >
-          New <span style={{ color: "#F31503" }}>Videos</span>
+          {selectedCategory} <span style={{ color: "#F31503" }}>Videos</span>
         </Typography>
         <Videos videos={[]} />
       </Box>
