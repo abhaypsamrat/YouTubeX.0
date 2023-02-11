2ce46d81c37bd2544c04aca5cc2b1660e27f5ba4
abhaypsamrat <abhay.p.samrat@gmail.com>
2023-02-11 19:31:57 +0530

    set videos

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { Stack, Box } from "@mui/material";
 
-const Videos = () => {
-  return <div>Videos</div>;
+const Videos = ({ videos }) => {
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.ChannelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
 };
 
 export default Videos;
