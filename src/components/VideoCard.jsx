9795dae05987b33a86fa8f37c762241a4e6375c2
abhaypsamrat <abhay.p.samrat@gmail.com>
2023-02-17 20:09:08 +0530

    design VideoCard

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -10,10 +10,23 @@ import {
   demoChannelTitle,
 } from "../utils/constants";
 
-const VideoCard = ({ video}) => {
-console.log(video)
-
-  return <div>VideoCard</div>;
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => {
+  return (
+    <Card>
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia
+          image={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title}
+          sx={{ width: 358, height: 180 }}
+        />
+      </Link>
+    </Card>
+  );
 };
 
 export default VideoCard;
